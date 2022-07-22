79e8dfc2d1d61778e2163db4c35d7cb3fbb564eb
Anjithashokan22 <109783769+Anjithashokan22@users.noreply.github.com>
2022-07-22 11:34:38 +0530

    Add files via upload
    rest2nd

---
+++ Restproject/myproject/api_basic/hello.py
@@ -0,0 +1,9 @@
+a=[1,2,3,4,5]
+def fun(a):
+    sum=0
+    for i in a:
+        sum+=i
+    return(sum)
+print(fun(a))
+
+
---
+++ Restproject/myproject/api_basic/urls.py
@@ -0,0 +1,13 @@
+# from django.contrib import admin
+from django.urls import path
+from .views import article_list,article_detail,ArticleAPIView,ArticleDetails,GenericAPIView
+
+urlpatterns = [
+    # path('article/',article_list),
+    path('article/',ArticleAPIView.as_view()),
+    # path('detail/<int:pk>/',article_detail),
+    path('detail/<int:id>/',ArticleDetails.as_view()),
+    path('generic/article/<int:id>/',GenericAPIView.as_view()),
+]
+
+
